8d163e0c7a772898563b3dba7cf5aa279f63ac1c
Vikram Bhogiraju <vikram.bhogiraju@gmail.com>
2015-12-09 01:46:02 +0530

    change to OO

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,15 @@
 //chat.js
+
+function ChatClient(name) {
+    this.name = name;
+    this.names.push(name);
+    this.getAllNames();
+}
+
+ChatClient.prototype.getAllNames = function() {
+    console.log(this.names.join(', '));
+}
+
+ChatClient.prototype.names = [];
+
+var c = new ChatClient("Vikram");
