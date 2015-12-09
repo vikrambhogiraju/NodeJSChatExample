9f7678c38a13eb0cfde1734eebcf6cda3659c90b
Vikram Bhogiraju <vikram.bhogiraju@gmail.com>
2015-12-10 01:46:00 +0530

    Client side - Display room list and send message done

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,1 @@
 //chat.js
-
-function ChatClient(name) {
-    this.name = name;
-    this.names.push(name);
-    this.getAllNames();
-}
-
-ChatClient.prototype.getAllNames = function() {
-    console.log(this.names.join(', '));
-}
-
-ChatClient.prototype.names = [];
-
-var c = new ChatClient("Vikram");
